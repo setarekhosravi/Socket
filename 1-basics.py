11073056a2892093f64a1a22da7a7115df48c745
Setare Khosravi <strhkhosravi@gmail.com>
2024-03-21 18:32:03 +0330

    add other basic instructions

--- 1-basics.py
+++ 1-basics.py
@@ -10,8 +10,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: # choose IPv4 
     print("server listening to incoming connections!")
 
     con, add = server.accept()
+    con.recv(1024)     # 1kb data wanted to recieve from client
+    con.sendall("Hi")  # send Hi message to client 
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client: # choose IPv4 as AF_INET
     client.bind((IP, PORT))                                       # choose TCP as SOCK_STREAM
-    
+    client.recv(1024)  # 1kb data wanted to recieve from server 
