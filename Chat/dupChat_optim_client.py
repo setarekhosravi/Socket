1f59269bfbb01fd15c753fbfcbde61934891b41d
Setare Khosravi <strhkhosravi@gmail.com>
2024-03-22 18:25:01 +0330

    optimal chatting client added

---
+++ Chat/dupChat_optim_client.py
@@ -0,0 +1,23 @@
+"""
+Socket Programming
+@author: STRH
+Created on 22 March, 2024
+    Basic implementation of chating communication,  duplex form
+    optimized version
+"""
+
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65440
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((HOST, PORT))
+    print("Connected to the server...")
+
+    while True:
+        message = input("Enter your message: ")
+        client.sendall(message.encode())
+
+        data = client.recv(1024)
+        print(f"Received message from the server: {data.decode('utf-8')}")
